eb93f991b9f019305b19ebc6628e337325f2bd62
Emily <emily.sali@outlook.com>
2023-09-04 12:19:23 +0100

    update threads

--- srcs/philosophers.c
+++ srcs/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 15:07:44 by esali             #+#    #+#             */
-/*   Updated: 2023/09/03 17:35:04 by esali            ###   ########.fr       */
+/*   Updated: 2023/09/03 19:15:05 by esali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ int	wait_for_fork(t_philo *p)
 	t_args	*args;
 
 	args = get_args();
+	if (args->philo_is_dead)
+		return (1);
 	while(p->prv->is_eating || p->nxt->is_eating)
 	{
 		gettimeofday(&(p->get_time), NULL);
+		printf("%i:	diff:%d\n",p->nr, p->get_time.tv_usec - p->last_eat.tv_usec);
 		if ((p->get_time.tv_usec - p->last_eat.tv_usec) > args->time_to_die \
 		&& p->nr_eat < args->min_nr_eat)
 		{
 			args->philo_is_dead = 1;
 			printf("%d %i died\n", p->get_time.tv_usec, p->nr);
-			return (1);
+			return (10);
 		}
-		usleep(10);
+		usleep(1);
 	}
 	if (args->philo_is_dead)
 		return (1);
@@ -68,6 +71,7 @@ void	*routine(void *philo)
 
 	p = (t_philo*) philo;
 	args = get_args();
+	gettimeofday(&(p->last_eat), NULL);
 	while (p->nr_eat < args->min_nr_eat) {
 		if (wait_for_fork(p))
 			return (NULL);
