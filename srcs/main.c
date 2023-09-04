16f73d1026080577184f9391bbc9c30a171fe1c8
Emily <emily.sali@outlook.com>
2023-09-04 12:26:15 +0100

    finish basic implementation

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 13:05:28 by esali             #+#    #+#             */
-/*   Updated: 2023/09/02 19:51:59 by esali            ###   ########.fr       */
+/*   Updated: 2023/09/03 17:33:20 by esali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,49 +26,3 @@ int	main(int argc, char **argv)
 	free_philos(get_args()->nr_philo);
 	return (0);
 }
-
-
-
-
-
-
-
-void*	print_thread(void *i)
-{
-	int	index;
-
-	index = *(int*) i;
-	printf("\nthis is a thread nr %i", index);
-	sleep(2);
-	printf("\nEND of %i", index);
-	free(i);
-	return (NULL);
-}
-
-void	create_threads(int *args)
-{
-	pthread_t	*t;
-	int			i;
-	int*		de_i;
-
-	t = (pthread_t *)malloc(sizeof(pthread_t) * args[0]);
-	i = 0;
-	while(i < args[0])
-	{
-		de_i = malloc(sizeof(int));
-		*de_i = i;
-		if (pthread_create(&t[i], NULL, &print_thread, de_i))
-			printf("\nFailed to create thread");
-		i++;
-	}
-	i = 0;
-	while (i < args[0])
-	{
-		if (pthread_join(t[i], NULL))
-			printf("\nFailed to join thread");
-		i++;
-	}
-	free(t);
-}
-
-
--- srcs/philosophers.c
+++ srcs/philosophers.c
@@ -6,11 +6,60 @@
 /*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 15:07:44 by esali             #+#    #+#             */
-/*   Updated: 2023/09/02 20:11:14 by esali            ###   ########.fr       */
+/*   Updated: 2023/09/03 17:35:04 by esali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
+
+int	wait_for_fork(t_philo *p)
+{
+	t_args	*args;
+
+	args = get_args();
+	while(p->prv->is_eating || p->nxt->is_eating)
+	{
+		gettimeofday(&(p->get_time), NULL);
+		if ((p->get_time.tv_usec - p->last_eat.tv_usec) > args->time_to_die \
+		&& p->nr_eat < args->min_nr_eat)
+		{
+			args->philo_is_dead = 1;
+			printf("%d %i died\n", p->get_time.tv_usec, p->nr);
+			return (1);
+		}
+		usleep(10);
+	}
+	if (args->philo_is_dead)
+		return (1);
+	return (0);
+}
+
+int	eat_sleep_think(t_philo *p)
+{
+	t_args	*args;
+
+	args = get_args();
+	pthread_mutex_lock(&(p->m));
+	if (!p->prv->is_eating && !p->nxt->is_eating)
+	{
+		p->is_eating = 1;
+		gettimeofday(&(p->last_eat), NULL);
+		printf("%d %i is eating\n", p->last_eat.tv_usec, p->nr);
+		usleep(args->time_to_eat);
+		p->is_eating = 0;
+	}
+	if (args->philo_is_dead)
+		return (1);
+	pthread_mutex_unlock(&(p->m));
+	gettimeofday(&(p->get_time), NULL);
+	printf("%d %i is sleeping\n", p->get_time.tv_usec, p->nr);
+	usleep(args->time_to_sleep);
+	if (args->philo_is_dead)
+		return (1);
+	gettimeofday(&(p->get_time), NULL);
+	printf("%d %i is thinking\n", p->get_time.tv_usec, p->nr);
+	return (0);
+}
 
 void	*routine(void *philo)
 {
@@ -19,29 +68,12 @@ void	*routine(void *philo)
 
 	p = (t_philo*) philo;
 	args = get_args();
-	if (args->min_nr_eat){
-		while (p->nr_eat < args->min_nr_eat) {
-			while(p->prv->is_eating || p->nxt->is_eating)
-			{
-			}
-			pthread_mutex_lock(&(p->m));
-			if (!p->prv->is_eating && !p->nxt->is_eating)
-			{
-				p->is_eating = 1;
-				gettimeofday(&(p->last_eat), NULL);
-				printf("%d %i is eating\n", p->last_eat.tv_usec, p->nr);
-				usleep(args->time_to_eat);
-				p->is_eating = 0;
-				p->nr_eat++;
-			}
-			pthread_mutex_unlock(&(p->m));
-			gettimeofday(&(p->get_time), NULL);
-			printf("%d %i is sleeping\n", p->get_time.tv_usec, p->nr);
-			usleep(args->time_to_sleep);
-			gettimeofday(&(p->get_time), NULL);
-			printf("%d %i is thinking\n", p->get_time.tv_usec, p->nr);
-		}
-		//printf("Philo nr. %i ate %i times\n", p->nr, p->nr_eat);
+	while (p->nr_eat < args->min_nr_eat) {
+		if (wait_for_fork(p))
+			return (NULL);
+		if (eat_sleep_think(p))
+			return (NULL);
+		p->nr_eat++;
 	}
 	return (NULL);
 }
