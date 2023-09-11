ab258e7b016f79fa1e292d51512db19e4c1a1c62
Emily <emily.sali@outlook.com>
2023-09-11 16:59:12 +0100

    adding loop

--- srcs/philos.c
+++ srcs/philos.c
@@ -6,7 +6,7 @@
 /*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:06:48 by esali             #+#    #+#             */
-/*   Updated: 2023/09/06 19:22:47 by esali            ###   ########.fr       */
+/*   Updated: 2023/09/07 16:45:20 by esali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,36 @@ void	*routine(void	*philo)
 
 	p = (t_philo*) philo;
 	gettimeofday(&(p->last_eat), NULL);
-	//printf("%i. created: %lu\n", p->nr, get_ms(p->last_eat, p->args));
-	pthread_mutex_lock(&(p->left->m));
-	p->left->is_busy = 1;
-	gettimeofday(&(p->get_time), NULL);
-	printf("%lu %i took a fork\n", get_ms(p->get_time, p->args), p->nr);
-	pthread_mutex_unlock(&(p->left->m));
-	pthread_mutex_lock(&(p->right->m));
-	p->left->is_busy = 1;
-	gettimeofday(&(p->get_time), NULL);
-	printf("%lu %i took a fork\n",get_ms(p->get_time, p->args), p->nr);
-	pthread_mutex_unlock(&(p->right->m));
-	gettimeofday(&(p->last_eat), NULL);
-	printf("%lu %i is eating\n", get_ms(p->last_eat, p->args), p->nr);
-	usleep(p->args->time_to_eat * 1000);
-	p->left->is_busy = 0;
-	p->right->is_busy = 1;
-	gettimeofday(&(p->get_time), NULL);
-	printf("%lu %i is thinking\n", get_ms(p->get_time, p->args), p->nr);
+	while (!(p->args->philo_is_dead))
+	{
+		pthread_mutex_lock(&(p->left->m));
+		p->left->is_busy++;
+		gettimeofday(&(p->get_time), NULL);
+		printf("%lu %i took left fork\n", get_ms(p->get_time, p->args), p->nr);
+		pthread_mutex_lock(&(p->right->m));
+		p->right->is_busy++;
+		gettimeofday(&(p->get_time), NULL);
+		printf("%lu %i took right fork\n",get_ms(p->get_time, p->args), p->nr);
+		gettimeofday(&(p->last_eat), NULL);
+		printf("%lu %i is eating\n", get_ms(p->last_eat, p->args), p->nr);
+		usleep(p->args->time_to_eat * 1000);
+		p->left->is_busy = 0;
+		p->right->is_busy = 0;
+		pthread_mutex_unlock(&(p->right->m));
+		pthread_mutex_unlock(&(p->left->m));
+		gettimeofday(&(p->get_time), NULL);
+		printf("%lu %i is sleeping\n", get_ms(p->get_time, p->args), p->nr);
+		usleep(p->args->time_to_sleep * 1000);
+		gettimeofday(&(p->get_time), NULL);
+		printf("%lu %i is thinking\n", get_ms(p->get_time, p->args), p->nr);
+		if (get_ms(p->get_time, p->args) - get_ms(p->last_eat, p->args) > p->args->time_to_die)
+		{
+			gettimeofday(&(p->get_time), NULL);
+			printf("%lu %i died\n", get_ms(p->get_time, p->args), p->nr);
+			p->args->philo_is_dead = 1;
+			return (NULL);
+		}
+	}
 	return (NULL);
 }
 
@@ -59,6 +71,7 @@ void	start_philos(t_philo **ps, t_args *args)
 	{
 		if (pthread_create(&(ps[i]->t), NULL, &routine, (void *) ps[i]))
 			return ;
+		usleep(100);
 		i++;
 	}
 	i = 0;
